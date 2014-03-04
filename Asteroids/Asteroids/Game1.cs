6780691cb9d880ad15403be2d64393763fa8edbd
HomersOdyssey <omirosproios@gmail.com>
2014-03-04 18:12:17 +0000

    Ship moves like the Ball lab exercise, when it goes through the edge of the screen it shows up on the opposite side. Peter licks balls.

--- Asteroids/Asteroids/Game1.cs
+++ Asteroids/Asteroids/Game1.cs
@@ -11,67 +11,96 @@ using Microsoft.Xna.Framework.Media;
 
 namespace Asteroids
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-
+        private Texture2D shipSprite;
+        private Vector2 shipPos = new Vector2();
+        private Vector2 shipV = new Vector2();
+        private int WIDTH, HEIGHT;
+        
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            WIDTH = GraphicsDevice.Viewport.Width;
+            HEIGHT = GraphicsDevice.Viewport.Height;
 
+            shipV = new Vector2(0, 0);
+            
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            shipSprite = Content.Load<Texture2D>("medspeedster");
+            shipPos = new Vector2((WIDTH - shipSprite.Width) / 2, (HEIGHT - shipSprite.Height) / 2);
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
+            
 
-            // TODO: Add your update logic here
+            KeyboardState keyboardState;
+            keyboardState = Keyboard.GetState();
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                shipV.X += 0.1f;
+            }
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                shipV.X -= 0.1f;
+            }
+            shipPos.X += shipV.X;
 
+            
+
+
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                shipV.Y -= 0.1f;
+            }
+            if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                shipV.Y += 0.1f;
+            }
+            shipPos.Y += shipV.Y;
+
+            //what happens when the ship goes to the left edge
+            if (shipPos.X + shipSprite.Width < 0)
+            {
+                shipPos.X = WIDTH;
+            }
+
+            //what happens when the ship goes to the right edge
+            if (shipPos.X > WIDTH)
+            {
+                shipPos.X = 0 - shipSprite.Width;
+            }
+
+            //what happens when the ship goes to the top edge
+            if (shipPos.Y + shipSprite.Height < 0)
+            {
+                shipPos.Y = HEIGHT;
+            }
+
+            //what happens when the ship goes to the bottom edge
+            if (shipPos.Y > HEIGHT)
+            {
+                shipPos.Y = 0 - shipSprite.Height;
+            }
             base.Update(gameTime);
         }
 
@@ -84,7 +113,9 @@ namespace Asteroids
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            spriteBatch.Begin();
+            spriteBatch.Draw(shipSprite, shipPos, Color.White);
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }
